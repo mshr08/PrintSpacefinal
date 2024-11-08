5b3f24708a2184875ea1e292950fd17c82356176
Harshini <harshini.m@etg.digital>
2024-11-08 17:42:05 +0530

    Day-4

--- index.js
+++ index.js
@@ -22,3 +22,41 @@ var x = setInterval(function(){
 
     }
 }, 1000);
+
+
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+    let initial_count = 0;
+    const final_count = counter.dataset.count;
+
+    let  counting = setInterval(updateCounting, 1);
+
+   function updateCounting() {
+
+        if (initial_count < 1000) {
+            initial_count += 5;
+            counter.innerText = initial_count;
+        }
+
+        if (initial_count >= 1000) {
+            initial_count += 100;
+            counter.innerText = initial_count;
+        }
+
+        if (initial_count >= 10000) {
+            initial_count += 10000;
+            counter.innerText = '$' + (initial_count / 10000).toFixed(0) + 'M';
+        }
+
+        if (initial_count >= 1000000) {
+            initial_count += 500000;
+            counter.innerText = (initial_count / 1000000).toFixed(0) + 'M+';
+        }
+        
+
+        if (initial_count >= final_count) {
+            clearInterval(counting);
+        }
+    }
+})
